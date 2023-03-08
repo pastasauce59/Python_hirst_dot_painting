f451d95811a989346bbdd18398a3457e61df4162
Michael Primavera <pastasauce59@gmail.com>
2023-03-08 14:24:51 -0500

    created a 10 x 10 hirst dot painting using generated colors from image.jpeg and randomly selecting a color from color list each time a dot is drawn.

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
-import colorgram
+# import colorgram
 
-colors = colorgram.extract('image.jpeg', 6)
-# print(colors[0].rgb[0])
+# colors = colorgram.extract('image.jpeg', 6)
 
-list_of_colors = []
-for color in colors:
-    rgb = (color.rgb[0], color.rgb[1], color.rgb[2])
-    list_of_colors.append(rgb)
+# list_of_colors = []
+# for color in colors:
+#     rgb = (color.rgb[0], color.rgb[1], color.rgb[2])
+#     list_of_colors.append(rgb)
 
-print(list_of_colors)
+# print(list_of_colors)
+
+color_list = [(250, 246, 243), (248, 245, 246), (212, 154, 97), (239, 246, 243), (52, 108, 132), (178, 78, 33)]
+
+import random
+import turtle as t
+painter = t.Turtle()
+t.colormode(255)
+
+painter.penup()
+
+def hirst_paint():
+    painter.speed('fastest')
+    yaxis = -250
+    while yaxis <= 200:
+        painter.setx(-250)
+        painter.sety(yaxis)
+        for paints in range(10):
+            painter.color(random.choice(color_list))
+            painter.dot(20)
+            painter.forward(50)
+        
+        yaxis += 50
+
+    painter.hideturtle()   
+
+hirst_paint()
+
+
+
+
+
+
+screen = t.Screen()
+screen.exitonclick()
