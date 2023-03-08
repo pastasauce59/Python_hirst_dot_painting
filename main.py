664e68d9ac4f0b071f0623c108ca096a99fcdfc9
Michael Primavera <pastasauce59@gmail.com>
2023-03-08 13:27:46 -0500

    Used colorgram module to extract colors from image.jpeg and then return them in a list as rgb tuples.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import colorgram
+
+colors = colorgram.extract('image.jpeg', 6)
+# print(colors[0].rgb[0])
+
+list_of_colors = []
+for color in colors:
+    rgb = (color.rgb[0], color.rgb[1], color.rgb[2])
+    list_of_colors.append(rgb)
+
+print(list_of_colors)
